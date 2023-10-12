cc9511e8961f23cf498956234bc0eb272281acdf
Ksisioss <54034242+Ksisioss@users.noreply.github.com>
2023-10-12 14:34:28 +0200

    Rebase everything to develop branch

---
+++ API_NODE/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require("cors");
+
+// extras imports
+const path = require('path');
+const fs = require('fs');
+
