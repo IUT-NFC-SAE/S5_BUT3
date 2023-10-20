be2cab97d07c18dd265a03ad70acbc67fc68207b
maxime <maxime.theveneau@gmail.com>
2023-10-20 14:44:30 +0200

    fix errors

--- SERVEUR_TCP/server/src/MainServer.java
+++ SERVEUR_TCP/server/src/MainServer.java
@@ -13,7 +13,7 @@ class MainServer  {
         this.port = port;
         conn = new ServerSocket(port,1);
         idThread = 1;
-        exchanger = new DataExchanger("http://localhost:4567/weatherapi", "mongodb://localhost:27017");
+        exchanger = new DataExchanger("http://localhost:1234/weatherapi", "mongodb://localhost:27017");
         // need to initializae mongo driver
         if (!exchanger.getMongoDriver().init()) {
             throw new IOException("cannot reach mongodb server and/or weatherapi database");
