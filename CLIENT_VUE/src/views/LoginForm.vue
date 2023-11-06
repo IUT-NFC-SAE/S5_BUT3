ffeb5483c765a453b0d9c971f5b6282776e9cc93
AntoinePR1 <titounet.perrin@gmail.com>
2023-11-06 10:28:35 +0100

    modification front

---
+++ CLIENT_VUE/src/views/LoginForm.vue
@@ -0,0 +1,35 @@
+<template>
+  <div style="background: red">
+    <h2>Connexion</h2>
+    <form @submit.prevent="login">
+      <div>
+        <UInput @change="email = $event.value" color="sky" variant="outline" name="input" placeholder="E-mail" />
+      </div>
+      <div>
+        <UInput @change="email = $event.value" color="sky" variant="outline" name="input" placeholder="Mot de passe" />
+      </div>
+      <button type="submit">Se connecter</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    login() {
+      // Vous pouvez ajouter ici la logique d'authentification, par exemple, en utilisant une API
+      console.log("Email:", this.email);
+      console.log("Mot de passe:", this.password);
+      // Réinitialisez les champs après la soumission du formulaire
+      this.email = "";
+      this.password = "";
+    },
+  },
+};
+</script>
