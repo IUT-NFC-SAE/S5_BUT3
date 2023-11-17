30dc35f01c56c35f1558dc602672b61d6bb57bd2
ehyostepbro <80098893+valow-psd@users.noreply.github.com>
2023-11-17 11:09:40 +0100

    Ajout de la connexion en TCP

--- ARDUINO/capteurs_station_meteo/capteurs_station_meteo.ino
+++ ARDUINO/capteurs_station_meteo/capteurs_station_meteo.ino
@@ -4,14 +4,12 @@
 #include <HTTPClient.h>
 #include <BH1750.h> // Bibliothèque pour le capteur de luminosité BH1750
 #include <ArduinoJson.h>
-
-
+#include <NTPClient.h>
+#include <WiFiUdp.h>
 
 // Trouver les capteurs corrects pour la pluie et le vent
 // Visiblement, pas besoin de bibliothèque pour la pluie
 #include <RainSensor.h> // Bibliothèque pour le capteur de pluie
-
-
 
 #include <AnalogSensor.h> // Bibliothèque pour le capteur de vent analogique
 
@@ -20,17 +18,19 @@
 // Changer le ssid, password, et l'adresse du serveur
 const char* ssid = "testingiot";
 const char* password = "testingiot";
-const char* serverIP = "192.168.0.2";
-const String serverURL = "/storedata.php"; // L'URL du serveur
+const char* serverIP = "127.0.0.1"; // mettre localhost
+const int serverPort = 3000 // mettre le port correspondant
+//const String serverURL = "/storedata.php"; // L'URL du serveur -> pas important si transfert en HTTP
 const String id = "1"; // ID du microcontrôleur
 
-StaticJsonDocument<1000> docTemp; 
-StaticJsonDocument<1000> docHum;
-StaticJsonDocument<1000> docPress;
-StaticJsonDocument<1000> docRain; 
+// StaticJsonDocument<1000> docTemp; 
+// StaticJsonDocument<1000> docHum;
+// StaticJsonDocument<1000> docPress;
+// StaticJsonDocument<1000> docRain; 
 
 
-
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "pool.ntp.org");
 
 BME280I2C bme;
 BH1750 lightSensor; // Capteur de luminosité
@@ -72,6 +72,33 @@ void setup() {
 }
 
 void loop() {
+
+  if (!client.connected()) {
+    connectToServer();
+  } else {
+  // Requete pour ajouter les modules à la bdd en TCP
+  // 1 + " " + uc + " " + liste des chipsets
+
+  //Ajout du uc
+  String uc = "ESP32".concat(id);
+
+  // On ajoute le BME280, RainSensor et BH1750 ... --> faire en sorte qu'il soit adaptable peu importe le type de sensor
+  String chipsetList[] = {"BME280", "BH1750", "RainSensor"};
+
+  for(String chipset in chipsetList){
+    String addModuleUrl = 1 + " " + uc + " " + chipset;
+
+    sendRequest(addModuleUrl);
+
+    // Attendre la réponse du serveur
+    String response = receiveResponse();
+    Serial.println("Server Response: " + response);
+
+    delay(1000);
+  }
+
+
+
   // Acquisition des mesures du BME280
   float temp(NAN), hum(NAN), pres(NAN);
   BME280::TempUnit tempUnit(BME280::TempUnit_Celsius);
@@ -84,7 +111,7 @@ void loop() {
   float windSpeed = windSensor.readValue(); // Lire la vitesse du vent depuis le capteur
 
   
-
+// Exemple requete HTTP --> visiblement on doit pas faire ça
 //{
 //  "type" : "TEMPERATURE",
 //  "date" : "2020-05-13",
@@ -92,34 +119,121 @@ void loop() {
 //  "module" : "$id",
 //}
 
-  JsonObject doc_0_Temp = docTemp.createNestedObject();
-  doc_0_Temp["type"] = "TEMPERATURE";
-  doc_0_Temp["date"] = ;
-  doc_0_Temp["value"] = String(temp);
-  doc_0_Temp["module"] = id;
+// Mise à jour de l'heure
+  timeClient.update();
+
+  // Récupération de l'heure actuelle
+  //Serial.println(timeClient.getFormattedTime());
+
+  // JsonObject doc_0_Temp = docTemp.createNestedObject();
+  // doc_0_Temp["type"] = "TEMPERATURE";
+  // // Pour récupérer la date, se connecter à un serveur distant et récupérer l'heure de ce serveur
+  // doc_0_Temp["date"] = String(Serial.println(timeClient.getFormattedTime()));
+  // doc_0_Temp["value"] = String(temp);
+  // doc_0_Temp["module"] = id;
+
+  // JsonObject doc_0_Hum = docTemp.createNestedObject();
+  // doc_0_Hum["type"] = "HUMIDITE";
+  // // Pour récupérer la date, se connecter à un serveur distant et récupérer l'heure de ce serveur
+  // doc_0_Hum["date"] = String(Serial.println(timeClient.getFormattedTime()));
+  // doc_0_Hum["value"] = String(hum);
+  // doc_0_Hum["module"] = id;
+
+  // JsonObject doc_0_Pres = docTemp.createNestedObject();
+  // doc_0_Pres["type"] = "PRESSION";
+  // // Pour récupérer la date, se connecter à un serveur distant et récupérer l'heure de ce serveur
+  // doc_0_Pres["date"] = String(Serial.println(timeClient.getFormattedTime()));
+  // doc_0_Pres["value"] = String(pres);
+  // doc_0_Pres["module"] = id;
+
+  // JsonObject doc_0_Rain = docTemp.createNestedObject();
+  // doc_0_Rain["type"] = "PLUIE";
+  // // Pour récupérer la date, se connecter à un serveur distant et récupérer l'heure de ce serveur
+  // doc_0_Rain["date"] = String(Serial.println(timeClient.getFormattedTime()));
+  // doc_0_Rain["value"] = String(isRain ? "1" : "0");
+  // doc_0_Rain["module"] = id;
+
+  // String jsonString1;
+  // serializeJson(doc_0_Temp, jsonString1);
+  // Serial.println(jsonString1);
+
+  // String jsonString2;
+  // serializeJson(doc_0_Hum, jsonString2);
+  // Serial.println(jsonString2);
+
+  // String jsonString3;
+  // serializeJson(doc_0_Pres, jsonString3);
+  // Serial.println(jsonString3);
+
+  // String jsonString4;
+  // serializeJson(doc_0_Rain, jsonString4);
+  // Serial.println(jsonString4);
 
 
+  // 2 pour l'ajout d'une valeur
+  // 2 + " "+ type de valeur mesuree + " " + val +" " + date + " " + id module
+  // temperature, humidite, pression, luminosité, pluie
+  // Envoi vers le serveur TCP 
 
 
   // Construire l'URL avec les valeurs des capteurs
-  String url = "/storedata.php?temp=" + String(temp) + "&humidity=" + String(hum) + "&pressure=" + String(pres)
-               + "&light=" + String(light) + "&rain=" + String(isRain ? "1" : "0") + "&windspeed=" + String(windSpeed);
+  //String url = "/storedata.php?temp=" + String(temp) + "&humidity=" + String(hum) + "&pressure=" + String(pres) + "&light=" + String(light) + "&rain=" + String(isRain ? "1" : "0") + "&windspeed=" + String(windSpeed);
+
+  String request1 = 2 + " " + "TEMPERATURE" + " " + String(temp) + " " + String(Serial.println(timeClient.getFormattedTime())) + " " + id;
+  String request2 = 2 + " " + "HUMIDITE" + " " + String(hum) + " " + String(Serial.println(timeClient.getFormattedTime())) + " " + id;
+  String request3 = 2 + " " + "PRESSION" + " " + String(pres) + " " + String(Serial.println(timeClient.getFormattedTime())) + " " + id;
+  String request4 = 2 + " " + "LIGHT" + " " + String(light) + " " + String(Serial.println(timeClient.getFormattedTime())) + " " + id;
+  String request5 = 2 + " " + "RAIN" + " " + String(isRain ? "1" : "0") + " " + String(Serial.println(timeClient.getFormattedTime())) + " " + id;
+
+
 
   // Effectuer la requête HTTP
-  HTTPClient http;
-  http.begin(serverIP, 80, url);
+  // for(int i=1; i<=5; i++){
+  //   HTTPClient http;
+  //   http.begin(serverIP, 80, "request".concat(i));
 
-  int httpResponseCode = http.GET();
+  //   int httpResponseCode = http.GET();
 
-  if (httpResponseCode > 0) {
-    String response = http.getString();
-    Serial.println("HTTP Response Code: " + String(httpResponseCode));
+  //   if (httpResponseCode > 0) {
+  //     String response = http.getString();
+  //     Serial.println("HTTP Response Code: " + String(httpResponseCode));
+  //     Serial.println("Server Response: " + response);
+  //   } else {
+  //     Serial.println("HTTP Request failed");
+  //   }
+
+  //   http.end();
+  // }
+
+  for(int i =1; i<=5; i++){
+    sendRequest("request".concat(i));
+
+    // Attendre la réponse du serveur
+    String response = receiveResponse();
     Serial.println("Server Response: " + response);
-  } else {
-    Serial.println("HTTP Request failed");
+    delay(1000);
+
   }
 
-  http.end();
+  }
+}
 
-  delay(1000);
+void connectToServer() {
+  if (client.connect(serverIP, serverPort)) {
+    Serial.println("Connected to server");
+    client.println(id); // Envoyer l'ID du microcontrôleur
+  }
+}
+
+void sendRequest(String request) {
+  client.println(request);
+}
+
+String receiveResponse() {
+  String response = "";
+  while (client.available()) {
+    char c = client.read();
+    response += c;
+  }
+  return response;
 }
