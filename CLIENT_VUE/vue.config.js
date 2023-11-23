5e92ec3cb8236b8b5d1c1cad1c6a2542caa91c07
maxime <maxime.theveneau@gmail.com>
2023-11-23 15:11:22 +0100

    update .env and vue.config.js

--- CLIENT_VUE/vue.config.js
+++ CLIENT_VUE/vue.config.js
@@ -1,4 +1,13 @@
-const { defineConfig } = require('@vue/cli-service')
+const dotenv = require('dotenv');
+const path = require('path');
+const { defineConfig } = require('@vue/cli-service');
+
+dotenv.config({ path: path.join(__dirname, '.env') });
+
 module.exports = defineConfig({
   transpileDependencies: true,
-})
+  devServer: {
+    port: process.env.VUE_APP_PORT,
+    host: process.env.VUE_APP_HOSTNAME,
+  },
+});
