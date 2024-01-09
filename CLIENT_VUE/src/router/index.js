05b7081b2e10960ad74faba5a16e55d7ca31ef3e
maxime <maxime.theveneau@gmail.com>
2024-01-09 16:44:18 +0100

    merge clients Vue

--- CLIENT_VUE/src/router/index.js
+++ CLIENT_VUE/src/router/index.js
@@ -1,37 +1,54 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import AppVue from '@/App.vue'
-import LoginView from '@/views/LoginView.vue'
-import StatsView from '@/views/StatsView.vue'
-
-Vue.use(VueRouter)
+import { createRouter, createWebHistory } from 'vue-router';
+import home from "@/views/home.vue";
+import moduleDetails from "@/views/moduleDetails.vue";
+import modules from "@/views/modules.vue";
+import authentication from "@/views/authentication.vue";
+import store from '@/store';
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component:  {AppVue}
-  },
-  {
-    path: '/about',
-    name: 'about',
-  },
-  {
-    path: '/login',
-    name: 'login',
-    component: LoginView
-  },
-  {
-    path: '/stats',
-    name: 'stats',
-    component: StatsView
-  },
-]
+    {
+        path: '/',
+        name: 'Home',
+        component: home,
+    },
+    {
+        path: '/auth',
+        name: 'Authentication',
+        component: authentication,
+    },
+    {
+        path: '/modules',
+        name: 'Modules',
+        component: modules,
+    },
+    {
+        path: '/module/:id',
+        name: 'ModuleDetails',
+        component: moduleDetails,
+        props: true,
+    },
+    {
+        path: '/:catchAll(.*)',
+        redirect: '/',
+    },
+];
 
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
-})
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
 
-export default router
+const checkUser = (to,from,next) => {
+    if (to.path !== '/auth' && !store.state.userModule.user) {
+        store.commit('popupModule/setError', "L'utilisateur n'est pas authentifié !")
+        next('/auth');
+    } else if(to.path === '/auth' && store.state.userModule.user) {
+        next('/');
+    } else {
+        next();
+    }
+}
+
+router.beforeEach(checkUser);
+
+export default router;
