969b40ff059889d6ea46adcfed01ed875a133363
maxime <maxime.theveneau@gmail.com>
2024-01-11 10:19:58 +0100

    update

--- CLIENT_VUE/src/services/axios.service.js
+++ CLIENT_VUE/src/services/axios.service.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import store from "@/store";
 
 const API = axios.create({
-    baseURL: "http://127.0.0.1:4567/weatherapi"
+    baseURL: "http://127.0.0.1:3000/weatherapi"
 });
 
 API.interceptors.request.use(
