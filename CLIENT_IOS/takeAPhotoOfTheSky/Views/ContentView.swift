085cba4ab72182bb4ccd56e3bf4dc7fd9afae368
Valow <valowmodz@gmail.Com>
2024-06-20 12:42:11 +0200

    Adding buttons and analysis

--- CLIENT_IOS/takeAPhotoOfTheSky/Views/ContentView.swift
+++ CLIENT_IOS/takeAPhotoOfTheSky/Views/ContentView.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import UIKit
 
 extension Data {
     mutating func append(_ string: String) {
@@ -10,89 +11,166 @@ extension Data {
 
 struct ContentView: View {
     @State private var image: UIImage?
+    @State private var imageID = UUID()
     @State private var isImagePickerPresented: Bool = false
-    
-    
+    @State private var isResultViewActive: Bool = false
+    @State private var prediction: String = ""
+    @State private var score: Double = 0.0
+    @State private var resetView: Bool = false
     
     func sendDataToAPI(imageData: Data) {
-        guard let url = URL(string: "https://send-a-photo-of-the-sky-saes-5.vercel.app/upload") else {
+        guard let url = URL(string: "http://192.168.233.17:5000/predict") else {
             print("Invalid URL")
             return
         }
         
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
+        request.setValue("application/octet-stream", forHTTPHeaderField: "Content-Type")
         
-        let boundary = "Boundary-\(UUID().uuidString)"
-        request.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+        // Ajouter les données de l'image au corps de la requête
+        request.httpBody = imageData
         
-        var body = Data()
-        body.append("--\(boundary)\r\n")
-        body.append("Content-Disposition: form-data; name=\"photo\"; filename=\"image.jpg\"\r\n")
-        body.append("Content-Type: image/jpeg\r\n\r\n")
-        body.append(imageData)
-        body.append("\r\n")
-        body.append("--\(boundary)--\r\n")
+        // Créer une session URLSession
+        let session = URLSession.shared
         
-        request.httpBody = body
-        
-        URLSession.shared.dataTask(with: request) { data, response, error in
+        // Créer la tâche de requête
+        let task = session.dataTask(with: request) { data, response, error in
             if let error = error {
-                print("Error sending to API: \(error.localizedDescription)")
+                print("Error occurred: \(error)")
+                return
+            }
+            guard let httpResponse = response as? HTTPURLResponse,
+                  httpResponse.statusCode == 200 else {
+                print("Invalid response from server")
                 return
             }
             
             if let data = data {
-                print("API Response: \(String(data: data, encoding: .utf8) ?? "")")
+                do {
+                    if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any],
+                       let prediction = json["prediction"] as? String,
+                       let score = json["score"] as? Double {
+                        DispatchQueue.main.async {
+                            self.prediction = prediction
+                            self.score = score
+                            self.isResultViewActive = true
+                        }
+                    }
+                } catch {
+                    print("Failed to parse JSON response")
+                }
             }
-        }.resume()
+        }
+        // Démarrer la tâche
+        task.resume()
     }
     
+    var body: some View {
+        NavigationView {
+            VStack {
+                Image(uiImage: UIImage(named: "app_logo")!)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 110, height: 110)
+                    .foregroundColor(.accentColor)
+                Spacer()
+                if let image = image, !resetView {
+                    Image(uiImage: image)
+                        .resizable()
+                        .scaledToFit()
+                        .padding()
+                        .id(imageID)
+                    
+                    Button("Send to API") {
+                        if let imageData = image.jpegData(compressionQuality: 0.5) {
+                            sendDataToAPI(imageData: imageData)
+                        }
+                    }
+                    .font(.custom("Manrope-Medium", size: 12))
+                    .frame(width: 150, height: 50)
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+                    .padding()
+                    
+                    Button("Reset") {
+                        resetView = true
+                        imageID = UUID()  // Change the imageID to force the view to update
+                    }
+                    .font(.custom("Manrope-Medium", size: 12))
+                    .frame(width: 150, height: 50)
+                    .background(Color.gray)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+                    .padding()
+                    
+                    NavigationLink(destination: ResultView(prediction: prediction, score: score), isActive: $isResultViewActive) {
+                        Text("Results")
+                    }
+
+                } else {
+                    Button("Take a Photo") {
+                        isImagePickerPresented.toggle()
+                        resetView = false
+                    }
+                    .font(.custom("Manrope-Medium", size: 12))
+                    .frame(width: 150, height: 50)
+                    .background(Color.green)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+                    .padding()
+                    .sheet(isPresented: $isImagePickerPresented) {
+                        ImagePicker(image: $image, isPresented: $isImagePickerPresented)
+                    }
+                }
+                Spacer()
+            }
+            .navigationTitle("Photo App")
+        }
+    }
+}
+
+struct ResultView: View {
+    var prediction: String
+    var score: Double
+    @Environment(\.presentationMode) var presentationMode
     
     var body: some View {
         VStack {
-            Image(uiImage: UIImage(named: "app_logo")!)
-                .resizable()
-                .aspectRatio(contentMode: .fill)
-                .frame(width: 110, height: 110)
-                .foregroundColor(.accentColor)
+            Text("Prediction: \(prediction)")
+                .font(.largeTitle)
+                .padding()
+            Text(String(format: "Score: %.2f", score))
+                .font(.title)
+                .padding()
+            
+            Circle()
+                .trim(from: 0, to: CGFloat(min(score / 100, 1.0)))
+                .stroke(Color.blue, lineWidth: 20)
+                .frame(width: 200, height: 200)
+                .rotationEffect(Angle(degrees: -90))
+                .padding()
+            
             Spacer()
-            if let image = image {
-                Image(uiImage: image)
-                    .resizable()
-                    .scaledToFit()
-                    .padding()
-                
-                Button("Send to API") {
-                    if let imageData = image.jpegData(compressionQuality: 0.5) {
-                        sendDataToAPI(imageData: imageData)
-                    }
-                }
-                .font(.custom("Manrope-Medium", size: 12))
-                .frame(width: 75, height: 35)
-                .buttonStyle(ThreeDButtonStyle(fillColor: Color(red: 233/255, green: 255/255, blue: 235/255)))
-                .previewLayout(.sizeThatFits)
-                .padding()
-            } else {
-                Button("Take a Photo") {
-                    isImagePickerPresented.toggle()
-                }
-                .font(.custom("Manrope-Medium", size: 12))
-                .frame(width: 75, height: 35)
-                .buttonStyle(ThreeDButtonStyle(fillColor: Color(red: 37/255, green: 150/255, blue: 190/255)))
-                .previewLayout(.sizeThatFits)
-                .padding()
-                .sheet(isPresented: $isImagePickerPresented) {
-                    ImagePicker(image: $image)
-                }
+            
+            Button("Back to Home") {
+                presentationMode.wrappedValue.dismiss()
             }
-            Spacer()
+            .font(.custom("Manrope-Medium", size: 12))
+            .frame(width: 150, height: 50)
+            .background(Color.red)
+            .foregroundColor(.white)
+            .cornerRadius(10)
+            .padding()
         }
+        .navigationTitle("Result")
     }
 }
 
 struct ImagePicker: UIViewControllerRepresentable {
     @Binding var image: UIImage?
+    @Binding var isPresented: Bool
     
     func makeCoordinator() -> Coordinator {
         Coordinator(parent: self)
@@ -119,7 +197,9 @@ struct ImagePicker: UIViewControllerRepresentable {
                 parent.image = uiImage
             }
             
-            picker.dismiss(animated: true)
+            picker.dismiss(animated: true) {
+                self.parent.isPresented = false
+            }
         }
     }
 }
